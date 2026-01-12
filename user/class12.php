52509856906bce8758d2189a4e78a53d6ce2eef3
Ankita-987 <pokharel.anki123@gmail.com>
2026-01-12 19:47:24 +0545

    Add files via upload

---
+++ user/class12.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Class 12</title>
+    <link rel="stylesheet" href="./ten.css">
+</head>
+<body>
+    <div class="flex">
+        <div class="main">
+            <h2>E-Learning </h2>
+        </div>
+         <div class="main">
+            <h3>Class 12 </h3>
+        </div>
+    </div>
+    <div class="header">
+       <h3>Find Your needed pdf, Question Here</h3>
+    </div>
+<div class="box">
+    <div class="card">
+        <a href="#"><img src="./img/question1.png" alt="Question Paper"></a><br>
+        <a href="#">Old is gold solution</a>
+    </div>
+    <div class="card">
+        <a href="#"><img src="./img/note.jpg" alt="Question Paper"></a><br>
+        <a href="#"> Subject wise notes</a>
+    </div>
+    <div class="card">
+        <a href="#"><img src="./img/pratice.JPG" alt="Question Paper"></a><br>
+        <a href="#">Pratice Question</a>
+    </div>
+    <div class="card">
+        <a href="#"><img src="./img/mcq.png" alt="Question Paper"></a><br>
+        <a href="#">MCQs</a>
+    </div>
+</div>
+<div class="box">
+    <div class="card">
+        <a href="#"><img src="./img/solution.jpg" alt="Question Paper"></a><br>
+        <a href="#">HISSAN set book solution</a>
+    </div>
+    <div class="card">
+        <a href="#"><img src="./img/qa section.jpg" alt="Question Paper"></a><br>
+        <a href="#">Q/A Section</a>
+    </div>
+    <div class="card">
+        <a href="#"><img src="./img/mock.jpg" alt="Question Paper"></a><br>
+        <a href="#">Mock exam</a>
+    </div>
+    <div class="card">
+        <a href="#"><img src="./img/profile.jpg" alt="Question Paper"></a><br>
+        <a href="#">Personal Info</a>
+    </div>
+</div>
+
+</body>
+</html>
